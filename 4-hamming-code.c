93261c59587a9416accc2dbab2ffa5a7661998f8
vikramN <nandimandalamvikram6@gmail.com>
2025-10-16 05:22:07 +0530

    written code

--- 4-hamming-code.c
+++ 4-hamming-code.c
@@ -1,5 +1,48 @@
-#include<stdio.h>
-int main(){
-    printf("hello world");
+#include <stdio.h>
+#include <math.h>
+
+// Calculate number of parity bits
+int getParityBits(int m) {
+    int r = 0;
+    while ((1 << r) < m + r + 1) r++;
+    return r;
+}
+
+// Generate Hamming code
+void generateHamming(int data[], int m) {
+    int r = getParityBits(m);
+    int n = m + r;
+    int hamming[20]; // enough size
+
+    // Place data and parity bits
+    for (int i = 1, j = 1; i <= n; i++) {
+        if ((i & (i - 1)) == 0) hamming[i] = 0; // parity position
+        else hamming[i] = data[j++];
+    }
+
+    // Calculate parity bits
+    for (int i = 1; i <= n; i <<= 1) {
+        int parity = 0;
+        for (int j = 1; j <= n; j++)
+            if (j & i) parity ^= hamming[j];
+        hamming[i] = parity;
+    }
+
+    // Print Hamming code
+    printf("Hamming code: ");
+    for (int i = 1; i <= n; i++) printf("%d", hamming[i]);
+    printf("\n");
+}
+
+int main() {
+    int m;
+    printf("Number of data bits: ");
+    scanf("%d", &m);
+
+    int data[20];
+    printf("Enter data bits: ");
+    for (int i = 1; i <= m; i++) scanf("%d", &data[i]);
+
+    generateHamming(data, m);
     return 0;
 }
