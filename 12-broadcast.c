73c1d446ca9e4403d2b6ca339761ba8b8c717c11
vikramN <nandimandalamvikram6@gmail.com>
2025-10-16 13:57:42 +0530

    all modified code

---
+++ 12-broadcast.c
@@ -0,0 +1,132 @@
+// /*
+// ------------------------------------------------------------
+//     AIM: Write a program to implement Broadcast Tree 
+//          by taking subnet of hosts.
+// ------------------------------------------------------------
+// */
+
+// #include <stdio.h>
+// #include <stdlib.h>
+
+// #define MAX 20
+
+// // Function to create and display broadcast tree
+// void createBroadcastTree(int hosts[MAX][MAX], int n) {
+//     printf("\nAdjacency Matrix (Subnet Representation):\n");
+//     for (int i = 0; i < n; i++) {
+//         for (int j = 0; j < n; j++) {
+//             printf("%d ", hosts[i][j]);
+//         }
+//         printf("\n");
+//     }
+
+//     printf("\nBroadcast Tree Traversal (using BFS):\n");
+//     int visited[MAX] = {0}, queue[MAX], front = 0, rear = 0;
+
+//     int start;
+//     printf("Enter the starting node (0 to %d): ", n - 1);
+//     scanf("%d", &start);
+
+//     visited[start] = 1;
+//     queue[rear++] = start;
+
+//     while (front < rear) {
+//         int node = queue[front++];
+//         printf("Node %d is broadcasting...\n", node);
+
+//         for (int i = 0; i < n; i++) {
+//             if (hosts[node][i] == 1 && !visited[i]) {
+//                 visited[i] = 1;
+//                 queue[rear++] = i;
+//             }
+//         }
+//     }
+// }
+
+// int main() {
+//     int hosts[MAX][MAX], n;
+
+//     printf("Enter number of hosts (nodes): ");
+//     scanf("%d", &n);
+
+//     printf("Enter adjacency matrix (1 if connected, 0 if not):\n");
+//     for (int i = 0; i < n; i++) {
+//         for (int j = 0; j < n; j++) {
+//             scanf("%d", &hosts[i][j]);
+//         }
+//     }
+
+//     createBroadcastTree(hosts, n);
+
+//     printf("\nBroadcast completed successfully!\n");
+//     return 0;
+// }
+
+// /*
+// ------------------------------------------------------------
+// OUTPUT (Example):
+
+// Enter number of hosts (nodes): 4
+// Enter adjacency matrix (1 if connected, 0 if not):
+// 0 1 1 0
+// 1 0 0 1
+// 1 0 0 1
+// 0 1 1 0
+
+// Adjacency Matrix (Subnet Representation):
+// 0 1 1 0
+// 1 0 0 1
+// 1 0 0 1
+// 0 1 1 0
+
+// Enter the starting node (0 to 3): 0
+// Broadcast Tree Traversal (using BFS):
+// Node 0 is broadcasting...
+// Node 1 is broadcasting...
+// Node 2 is broadcasting...
+// Node 3 is broadcasting...
+
+// Broadcast completed successfully!
+// ------------------------------------------------------------
+// */
+// 
+
+
+
+#include<stdio.h>
+#include<stdlib.h>
+
+#define max 20
+
+void createBroadcastTree(int hosts[max][max], int n){
+    printf("\n boradcast tree  traversal (using BFS):\n");
+    int visited[max] = {0}, queue[max], front = 0, rear = 0;
+    int start;
+    printf("\n enter start node: ");
+    scanf("%d", &start);
+    visited[start]=1;
+    queue[rear++]= start;
+    while(front< rear){
+        int node = queue[front++];
+        printf("node %d is braodcasting....\n", node);
+        for(int i=0;i<n;i++){
+            if(hosts[node][i] == 1 && !visited[i]){
+                visited[i]=1;
+                queue[rear++]= i;
+            }
+        }
+    }
+}
+int main(){
+    int hosts[max][max], n;
+    printf("Enter number of hosts (nodes): ");
+    scanf("%d", &n);
+    printf("enter the adjacency matrix (1 if connected, 0 if not):\n");
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            scanf("%d", &hosts[i][j]);
+        }
+    }
+    createBroadcastTree(hosts, n);
+    return 0;
+}
--- 4-hamming-code.c
+++ 4-hamming-code.c
@@ -1,48 +1,94 @@
-#include <stdio.h>
-#include <math.h>
+// #include <stdio.h>
+// #include <math.h>
 
-// Calculate number of parity bits
-int getParityBits(int m) {
-    int r = 0;
-    while ((1 << r) < m + r + 1) r++;
+// // Calculate number of parity bits
+// int getParityBits(int m) {
+//     int r = 0;
+//     while ((1 << r) < m + r + 1) r++;
+//     return r;
+// }
+
+// // Generate Hamming code
+// void generateHamming(int data[], int m) {
+//     int r = getParityBits(m);
+//     int n = m + r;
+//     int hamming[20]; // enough size
+
+//     // Place data and parity bits
+//     for (int i = 1, j = 1; i <= n; i++) {
+//         if ((i & (i - 1)) == 0) hamming[i] = 0; // parity position
+//         else hamming[i] = data[j++];
+//     }
+
+//     // Calculate parity bits
+//     for (int i = 1; i <= n; i <<= 1) {
+//         int parity = 0;
+//         for (int j = 1; j <= n; j++)
+//             if (j & i) parity ^= hamming[j];
+//         hamming[i] = parity;
+//     }
+
+//     // Print Hamming code
+//     printf("Hamming code: ");
+//     for (int i = 1; i <= n; i++) printf("%d", hamming[i]);
+//     printf("\n");
+// }
+
+// int main() {
+//     int m;
+//     printf("Number of data bits: ");
+//     scanf("%d", &m);
+
+//     int data[20];
+//     printf("Enter data bits: ");
+//     for (int i = 1; i <= m; i++) scanf("%d", &data[i]);
+
+//     generateHamming(data, m);
+//     return 0;
+// }
+
+
+
+
+#include<stdio.h>
+#include<math.h>
+
+int getparitybits(int m){
+    int r= 0;
+    while((1<<r) < m+r+1)
+    r++;
     return r;
 }
-
-// Generate Hamming code
-void generateHamming(int data[], int m) {
-    int r = getParityBits(m);
-    int n = m + r;
-    int hamming[20]; // enough size
-
-    // Place data and parity bits
-    for (int i = 1, j = 1; i <= n; i++) {
-        if ((i & (i - 1)) == 0) hamming[i] = 0; // parity position
+void generateHamming(int data[], int m){
+    int r= getparitybits(m);
+    int n= m+r;
+    int hamming[m*m];
+    for(int i=1, j=1; i<=n;i++){
+        if((i&(i-1)) == 0) hamming[i] = 0;
         else hamming[i] = data[j++];
     }
 
-    // Calculate parity bits
-    for (int i = 1; i <= n; i <<= 1) {
+    for(int i =1; i<=n; i<<=1){
         int parity = 0;
-        for (int j = 1; j <= n; j++)
-            if (j & i) parity ^= hamming[j];
+        for(int j=1; j<=n; j++){
+            if(j&i) parity ^= hamming[j];
         hamming[i] = parity;
+        }
     }
-
-    // Print Hamming code
-    printf("Hamming code: ");
-    for (int i = 1; i <= n; i++) printf("%d", hamming[i]);
-    printf("\n");
+    printf("hamming code: ");
+    for(int i =1; i<=n;i++){
+        printf("%d", hamming[i]);
+    }
 }
-
-int main() {
+int main(){
     int m;
-    printf("Number of data bits: ");
+    printf("Number of data bits:");
     scanf("%d", &m);
-
-    int data[20];
-    printf("Enter data bits: ");
-    for (int i = 1; i <= m; i++) scanf("%d", &data[i]);
-
+    int data[m*m];
+    printf("enter data bits: ");
+    for(int i=1; i<=m; i++){
+        scanf("%d",&data[i]);
+    }
     generateHamming(data, m);
     return 0;
 }
